89c7b9ebb802b91fdf78a9cdee45d841ad6ddaf6
Fristaylo <andru_2004@mail.ru>
2025-06-22 03:36:48 +0400

    Fix icons in Skill-card

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import Block from './Block.vue';
-import Svg from './Svg.vue';
 import type { Skill } from '../data/skill';
 
 const props = defineProps<{
@@ -9,21 +8,27 @@ const props = defineProps<{
 </script>
 
 <template>
-	<Block>
+	<Block class="flex gap-5 flex-col">
 		<div class="flex items-center gap-3">
-			<Svg :icon="props.skill.icon" :size="25"></Svg>
-			<h2 class="font-bold text-2xl">{{ props.skill.title }}</h2>
+			<div
+				v-html="props.skill.icon"
+				:style="{ width: '25px', height: '25px' }"
+			></div>
+
+			<h2 class="font-bold text-gray-600 text-2xl">
+				{{ props.skill.title }}
+			</h2>
 		</div>
 		<p>{{ props.skill.description }}</p>
-		<div class="flex items-center gap-2 mt-5">
-			<p>skill</p>
+		<div class="flex items-center gap-2">
+			<p class="font-medium">Скилл</p>
 			<div class="w-full h-2 bg-gray-200 rounded-md">
 				<div
 					class="h-full bg-orange-500 rounded-md"
 					:style="{ width: props.skill.level + '%' }"
 				></div>
 			</div>
-			<p>{{ props.skill.level }}%</p>
+			<p class="font-medium">{{ props.skill.level }}%</p>
 		</div>
 	</Block>
 </template>
